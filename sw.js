84e208c4a4a7a79431f33c9048a7ffc78110cf63
Seeker D. Code <seekerdcode@gmail.com>
2026-08-26 14:53:11 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -30,7 +30,7 @@ const ASSETS = [
     './images/food.webp',
     './images/folk.webp',
     './images/comics.webp',
-    './images/wild.webp',
+    './images/funny.webp',
     './images/Italia.webp',
     './images/logo FV.png'
 ];
@@ -38,10 +38,10 @@ const ASSETS = [
 
 // Installazione e cache delle risorse
 self.addEventListener('install', (e) => {
+    self.skipWaiting(); // Forza l'attivazione immediata del nuovo SW
     e.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
     );
-    self.skipWaiting();
 });
 
 // Attivazione e pulizia vecchie cache
