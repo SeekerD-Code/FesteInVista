47dd756629e5cf9c4c2dec3063546e44e75c627a
Seeker D. Code <seekerdcode@gmail.com>
2026-08-26 16:58:45 +0200

    Update map-preferiti.js

--- js/map-preferiti.js
+++ js/map-preferiti.js
@@ -14,7 +14,7 @@ function getPngCategoria(categoria) {
     if (cat.includes('folk') || cat.includes('tradizion') || cat.includes('rievocazion') || cat.includes('storica')) {
         return "images/folk.png";
     }
-    return "images/funny.webp";
+    return "images/funny.png";
 }
 
 function formattaDataItaliana(dataStr) {
